0180a6407f1384eca1c79e5fa8ea9c3c8a951a7c
erictapen <justin.humm@posteo.de>
2016-02-10 17:47:08 +0100

    clean mvn repo

---
+++ src/main/java/langtest/EntryPoint.java
@@ -0,0 +1,11 @@
+package treetest;
+
+import processing.core.PApplet;
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+		PApplet.main("langtest.TreeDraw", args);   
+	}
+
+}
