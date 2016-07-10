a120deef777f13aa5dd0a08f64f695aa5469af72
Justin Humm <justin.humm@posteo.de>
2016-07-10 23:13:21 +0200

    ForLoop

--- src/main/java/treelang/parser/PNode.java
+++ src/main/java/treelang/parser/PNode.java
@@ -3,6 +3,7 @@ package treelang.parser;
 import java.util.ArrayList;
 
 import treelang.picture.TDivide;
+import treelang.picture.TForLoop;
 import treelang.picture.TIdentifier;
 import treelang.picture.TLambda;
 import treelang.picture.TList;
@@ -69,6 +70,10 @@ public class PNode {
 			if (picChilds.size() != 3)
 				throw new SyntaxErrorException();
 			return new TLambda(this.children.get(0).getCaption(), picChilds.get(1), picChilds.get(2));
+		case "For":
+			if (picChilds.size() != 3)
+				throw new SyntaxErrorException();
+			return new TForLoop(this.children.get(0).getCaption(), picChilds.get(1), picChilds.get(2));
 		case "+":
 			if(picChilds.size() != 2)
 				throw new SyntaxErrorException();
