e7286fc5e13dfcba01aa2b1880e77a2be43b2ef4
Justin Humm <justin.humm@posteo.de>
2016-07-11 19:23:12 +0200

    bugfix

--- src/main/java/treelang/picture/TTranslate.java
+++ src/main/java/treelang/picture/TTranslate.java
@@ -68,11 +68,11 @@ public class TTranslate implements TPicture {
 
 	@Override
 	public void draw(PApplet p) {
-		if (getPic().getClass() == TPoint.class) {
-			p.pushMatrix();
+		//if (getPic().getClass() == TPoint.class) {
 			p.translate(getNum1().getNumber().getValue(), getNum2().getNumber().getValue());
+			p.pushMatrix();
 			getPic().draw(p);
-		}
+		//}
 		p.popMatrix();
 	}
 
